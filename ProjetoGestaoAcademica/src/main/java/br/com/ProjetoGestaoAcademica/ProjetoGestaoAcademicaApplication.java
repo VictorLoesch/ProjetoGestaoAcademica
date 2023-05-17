472bf20de780288297b27ba8f9149828ea4181ef
Victor <Victor@10.21.32.72>
2023-05-16 22:33:44 -0300

    Criação do Projeto

---
+++ ProjetoGestaoAcademica/src/main/java/br/com/ProjetoGestaoAcademica/ProjetoGestaoAcademicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ProjetoGestaoAcademica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGestaoAcademicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGestaoAcademicaApplication.class, args);
+	}
+
+}
